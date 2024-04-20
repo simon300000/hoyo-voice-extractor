7b183c030683d5f759aac2c6d7ad29e87a0b49d0
simon3000 <simon30002021@icloud.com>
2024-04-20 15:27:41 +0200

    export

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import { spawn } from 'child_process'
 
 const UNPACKER_PATH = join(import.meta.dirname, 'Wwise-Unpacker')
 const UNPACKER = join(UNPACKER_PATH, 'unpack_wav.bat')
+const WAV_PATH = join(UNPACKER_PATH, 'dest_wav')
+const WEM_PATH = join(UNPACKER_PATH, 'dest_raw')
 export const PCK_DESTINATION = join(UNPACKER_PATH, 'Game_Files')
 
 const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
@@ -44,3 +46,9 @@ export const unpack = () => new Promise<void>((resolve, reject) => {
     }
   })
 })
+
+export const exportFiles = async (folder: string) => {
+  await rename(WAV_PATH, join(folder, 'wav'))
+  await rename(WEM_PATH, join(folder, 'wem'))
+}
+
