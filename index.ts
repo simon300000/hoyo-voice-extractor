0682549919bb0b9eff0e181de67c860caafa9f7b
simon3000 <simon30002021@icloud.com>
2024-04-24 14:45:41 +0200

    parallel unpack

--- index.ts
+++ index.ts
@@ -1,82 +1,63 @@
-import { readdir, stat, rename, unlink } from 'fs/promises'
+import { readdir, stat, mkdir } from 'fs/promises'
 import { join, extname } from 'path'
-import { spawn } from 'child_process'
 
 import { fnv1 } from './fnvjs/src/index.js'
 
-const UNPACKER_PATH = join(import.meta.dirname, 'Wwise-Unpacker')
-const UNPACKER = join(UNPACKER_PATH, 'unpack_wav.bat')
-const WAV_PATH = join(UNPACKER_PATH, 'dest_wav')
-const WEM_PATH = join(UNPACKER_PATH, 'dest_raw')
-export const PCK_DESTINATION = join(UNPACKER_PATH, 'Game_Files')
+import { parallelExecute, unpackPCK, convertWEM } from './tools.js'
 
 const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 
 export const checkResultFolder = async (folder: string) => {
   const files = await readdir(folder)
   const no = ['wav', 'wem']
-  if (no.some(f => files.includes(f))) {
+  const badFiles = no.filter(f => files.includes(f))
+  if (badFiles.length) {
     console.log('Result folder already exists.')
     console.log(`Please remove the following files/folders from ${folder}:`)
-    console.log(no.join('\n'))
+    console.log(badFiles.join('\n'))
     process.exit(1)
   }
 }
 
-const movePCK = async (folder: string) => {
-  let i = 0
-  console.log(`Reading ${folder}...`)
+export const findFiles = async (folder: string, ext: string) => {
   const files = await readdir(folder)
+  const result = [] as string[]
+  const resultPromise = [] as Promise<string[]>[]
   for (const file of files) {
     const filePath = join(folder, file)
     const fileStat = await stat(filePath)
     if (fileStat.isDirectory()) {
-      i += await movePCK(filePath)
+      resultPromise.push(findFiles(filePath, ext))
     } else {
-      if (extname(file) === '.pck') {
-        i++
-        const newFile = random() + '.pck'
-        await rename(filePath, join(PCK_DESTINATION, newFile))
+      if (extname(file) === ext) {
+        result.push(filePath)
       }
     }
   }
-  return i
+  return [...result, ...(await Promise.all(resultPromise)).flat()]
 }
 
-export const importPCK = async (folder: string) => {
-  const i = await movePCK(folder)
-  console.log(`Moved ${i} files.`)
+export const unpack = async (source: string, wemFolder: string, wavFolder: string) => {
+  await mkdir(wemFolder, { recursive: true })
+  await mkdir(wavFolder, { recursive: true })
+  const pcks = await findFiles(source, '.pck')
+  console.log(`Found ${pcks.length} pck files.`)
+  console.log('Unpacking...')
+  await parallelExecute(pcks.map(pck => async () => {
+    const name = random()
+    const wem = join(wemFolder, name)
+    const wav = join(wavFolder, name)
+    await mkdir(wem)
+    await mkdir(wav)
+    await unpackPCK(pck, wem)
+  }))
+  const wems = await findFiles(wemFolder, '.wem')
+  console.log(`Found ${wems.length} wem files.`)
+  console.log('Converting...')
+  await parallelExecute(wems.map(wem => async () => {
+    const wavPath = wem.replace(wemFolder, wavFolder).replace('.wem', '.wav')
+    await convertWEM(wem, wavPath)
+  }), 1)
 }
-
-export const cleanPCK = async () => {
-  let i = 0
-  const files = await readdir(PCK_DESTINATION)
-  for (const file of files) {
-    if (extname(file) === '.pck') {
-      i++
-      await unlink(join(PCK_DESTINATION, file))
-    }
-  }
-  console.log(`Removed ${i} files.`)
-}
-
-export const unpack = () => new Promise<void>((resolve, reject) => {
-  const child = spawn(UNPACKER, { cwd: UNPACKER_PATH, stdio: 'inherit' })
-
-  child.on('close', code => {
-    if (code === 0) {
-      resolve()
-    } else {
-      reject(code)
-    }
-  })
-})
-
-export const exportFiles = async (folder: string) => {
-  await rename(WAV_PATH, join(folder, 'wav'))
-  await rename(WEM_PATH, join(folder, 'wem'))
-  console.log(`Exported files to ${folder}.`)
-}
-
 
 export const encodeFNV64 = (input: string) => fnv1(input.toLowerCase(), 64).toString(16).padStart(16, '0')
---
+++ tools.ts
@@ -0,0 +1,47 @@
+import { cpus } from 'os'
+import { join } from 'path'
+import { spawn } from 'child_process'
+
+const UNPACKER_PATH = join(import.meta.dirname, 'Wwise-Unpacker')
+const TOOLS_PATH = join(UNPACKER_PATH, 'Tools')
+const QUICKBMS = join(TOOLS_PATH, 'quickbms.exe')
+const WWISE_PCK_EXTRACTOR = join(TOOLS_PATH, 'wwise_pck_extractor.bms')
+const VGMSTREAM_CLI = join(TOOLS_PATH, 'vgmstream-cli.exe')
+
+export const parallelExecute = async (tasks: (() => Promise<void>)[], multiplier = 2) => {
+  const cpusCount = cpus().length * multiplier
+  const workers = [] as Promise<void>[]
+  let taskDone = 0
+  const taskDiv10 = Math.floor(tasks.length / 10)
+  for (let i = 0; i < cpusCount; i++) {
+    workers.push((async () => {
+      while (tasks.length) {
+        await tasks.shift()?.()
+        taskDone++
+        if (taskDone % taskDiv10 === 0) {
+          console.log(`${Math.floor(taskDone / taskDiv10) * 10}%`)
+        }
+      }
+    })())
+  }
+  await Promise.all(workers)
+}
+
+const execute = (command: string, args: string[]) => new Promise<void>((resolve, reject) => {
+  const process = spawn(command, args)
+  let stdout = ''
+  let stderr = ''
+  process.stdout.on('data', data => stdout += data)
+  process.stderr.on('data', data => stderr += data)
+  process.on('close', code => {
+    if (code === 0) {
+      resolve()
+    } else {
+      reject(new Error(`${command} exited with code ${code}\n${stdout}\n${stderr}`))
+    }
+  })
+})
+
+export const unpackPCK = async (pck: string, output: string) => execute(QUICKBMS, ['-q', '-k', WWISE_PCK_EXTRACTOR, pck, output])
+
+export const convertWEM = async (wem: string, wav: string) => execute(VGMSTREAM_CLI, ['-o', wav, wem])
