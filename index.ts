f2e02697e04454b49bbf0d72c8511d7e19a50cba
simon3000 <simon30002021@icloud.com>
2024-04-25 17:29:12 +0200

    unpack bnk

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { readdir, stat, mkdir } from 'fs/promises'
+import { readdir, stat, mkdir, rename } from 'fs/promises'
 import { join, extname, dirname } from 'path'
 
 import { fnv1 } from './fnvjs/src/index.js'
 
-import { parallelExecute, unpackPCK, convertWEM } from './tools.js'
+import { parallelExecute, unpackPCK, convertWEM, unpackBNK } from './tools.js'
 
 const random = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 
@@ -48,30 +48,51 @@ export const createFolders = async (files: string[], existing = new Set<string>(
   }
 }
 
+const convertWEMs = async (wems: string[], wemFolder: string, wavFolder: string) => {
+  await createFolders(wems.map(wem => wem.replace(wemFolder, wavFolder)))
+  await parallelExecute(wems.map(wem => async () => {
+    const wavPath = wem.replace(wemFolder, wavFolder).replace('.wem', '.wav')
+    await convertWEM(wem, wavPath)
+  }), 1)
+}
+
 export const unpackPCKs = async (source: string, wemFolder: string, wavFolder: string) => {
   await mkdir(wemFolder, { recursive: true })
   await mkdir(wavFolder, { recursive: true })
   const pcks = await findFiles(source, '.pck')
   console.log(`Found ${pcks.length} pck files.`)
   console.log('Unpacking...')
-  const wavCreatedFolders = new Set<string>()
   await parallelExecute(pcks.map(pck => async () => {
     const name = random()
     const wem = join(wemFolder, name)
     const wav = join(wavFolder, name)
     await mkdir(wem)
     await mkdir(wav)
-    wavCreatedFolders.add(wav)
     await unpackPCK(pck, wem)
   }))
   const wems = await findFiles(wemFolder, '.wem')
   console.log(`Found ${wems.length} wem files.`)
   console.log('Converting...')
-  await createFolders(wems.map(wem => wem.replace(wemFolder, wavFolder)), wavCreatedFolders)
-  await parallelExecute(wems.map(wem => async () => {
-    const wavPath = wem.replace(wemFolder, wavFolder).replace('.wem', '.wav')
-    await convertWEM(wem, wavPath)
-  }), 1)
+  await convertWEMs(wems, wemFolder, wavFolder)
+}
+
+export const unpackBNKs = async (source: string, wemFolder: string, wavFolder: string) => {
+  await mkdir(wemFolder, { recursive: true })
+  await mkdir(wavFolder, { recursive: true })
+  const bnks = await findFiles(source, '.bnk')
+  console.log(`Found ${bnks.length} bnk files.`)
+  console.log('Unpacking...')
+  await Promise.all(bnks.map(bnk => bnk.replace(source, wemFolder)).map(folder => mkdir(folder, { recursive: true })))
+  await parallelExecute(bnks.map(bnk => async () => unpackBNK(bnk, bnk.replace(source, wemFolder))))
+
+  console.log('Renaming wavs to wems...')
+  const wavsWems = await findFiles(wemFolder, '.wav')
+  await Promise.all(wavsWems.map(wavWem => rename(wavWem, wavWem.replace('.wav', '.wem'))))
+
+  const wems = await findFiles(wemFolder, '.wem')
+  console.log(`Found ${wems.length} wem files.`)
+  console.log('Converting...')
+  await convertWEMs(wems, wemFolder, wavFolder)
 }
 
 export const encodeFNV64 = (input: string) => fnv1(input.toLowerCase(), 64).toString(16).padStart(16, '0')
--- tools.ts
+++ tools.ts
@@ -1,5 +1,5 @@
 import { cpus } from 'os'
-import { join } from 'path'
+import { join, dirname } from 'path'
 import { spawn } from 'child_process'
 
 const UNPACKER_PATH = join(import.meta.dirname, 'Wwise-Unpacker')
@@ -7,6 +7,7 @@ const TOOLS_PATH = join(UNPACKER_PATH, 'Tools')
 const QUICKBMS = join(TOOLS_PATH, 'quickbms.exe')
 const WWISE_PCK_EXTRACTOR = join(TOOLS_PATH, 'wwise_pck_extractor.bms')
 const VGMSTREAM_CLI = join(TOOLS_PATH, 'vgmstream-cli.exe')
+const BNKEXTR = join(TOOLS_PATH, 'bnkextr.exe')
 
 export const parallelExecute = async (tasks: (() => Promise<void>)[], multiplier = 2) => {
   const cpusCount = cpus().length * multiplier
@@ -27,8 +28,8 @@ export const parallelExecute = async (tasks: (() => Promise<void>)[], multiplier
   await Promise.all(workers)
 }
 
-const execute = (command: string, args: string[]) => new Promise<void>((resolve, reject) => {
-  const process = spawn(command, args)
+const execute = (command: string, args: string[], cwd?: string) => new Promise<void>((resolve, reject) => {
+  const process = spawn(command, args, { cwd })
   let stdout = ''
   let stderr = ''
   process.stdout.on('data', data => stdout += data)
@@ -45,3 +46,5 @@ const execute = (command: string, args: string[]) => new Promise<void>((resolve,
 export const unpackPCK = async (pck: string, output: string) => execute(QUICKBMS, ['-q', '-k', WWISE_PCK_EXTRACTOR, pck, output])
 
 export const convertWEM = async (wem: string, wav: string) => execute(VGMSTREAM_CLI, ['-o', wav, wem])
+
+export const unpackBNK = async (bnk: string, output: string) => execute(BNKEXTR, [bnk], output)
